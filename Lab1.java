9d7c3e9ed668f9b7910aceda42990b65594d7274
Zsanguiamba <Zadenmoise@gmail.com>
2024-09-26 15:05:59 -0400

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,17 @@
+
+/**
+ * Lab 1 Assignment- Draw a traingle
+ *
+ * @author (Zaden Sanguiamba)
+ * @version (8/21/2024)
+ */
+public class Lab1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a triangle.");
+        System.out.println("  *");
+        System.out.println(" ***");
+        System.out.println("*****");
+    }
+}
